44610139412bce719fd145a688a404337a513042
Selin333 <mail4studying@yandex.ru>
2022-05-17 21:10:25 +0300

    Попытка сделать перевод 2

--- laba3.py
+++ laba3.py
@@ -1,15 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 
 image = Image.open('orig.jpg')
 im2arr = np.array(image)
-# print(im2arr)
+perem = image.load()
+draw =ImageDraw.Draw(image)
+draw.point((1,1),(255,255,255))
+# image.show()
+rgb = list(perem[1,1])
+print(type(rgb[0]))
+#print(im2arr)
 # arr2im = Image.fromarray(im2arr)
 # image.show()
-for i in im2arr:
-    for j in i:
-        for k in j:
-            print(type(k))
-            im2arr[i][j][k] = int(bin(k)[2:])
-            print(k)
-print(im2arr)
+# for i in im2arr:
+#     for j in i:
+#         for k in j:
+#
+#             arrara[i][j][k].append(bin(k))
+#             print(k)
