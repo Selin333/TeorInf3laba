1a81ae8c3f6139270ffa382973d7fac63a99b4be
Selin333 <mail4studying@yandex.ru>
2022-05-26 12:25:37 +0300

    работа с файлом картинки и 1 2 часть готовые(заного)

--- laba3.py
+++ laba3.py
@@ -1,21 +1,76 @@
-from PIL import Image, ImageDraw
+from tkinter import filedialog as fd
+from tkinter import *
+import os
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import random
 import numpy as np
+from sympy import *
+import math
 
-image = Image.open('orig.jpg')
-im2arr = np.array(image)
-perem = image.load()
-# draw =ImageDraw.Draw(image)
-# draw.point((1,1),(255,255,255))
-# image.show()
-# rgb = list(perem[1,1])
-# print(rgb)
-# print(bin(rgb[0]))
-#print(im2arr)
-# arr2im = Image.fromarray(im2arr)
-# image.show()
-# for i in im2arr:
-#     for j in i:
-#         for k in j:
+name = ""
+def callback():
+    ftypes = [("Картинки",".jpg")]
+    name = fd.askopenfilename(filetypes = ftypes)
+    return name
+
+def call():
+    global name
+    name = callback()
+
+
+def oshibaisya2(opa1):
+
+    rng=list(range(0,19))
+    rng1=random.sample(rng,2)
+    for i in range(len(rng1)):
+        if (opa1[rng1[i]])=="1":
+            opa1 = opa1[:rng1[i]] + "0" + opa1[rng1[i] + 1:]
+        else:
+            opa1 = opa1[:rng1[i]] + "1" + opa1[rng1[i] + 1:]
+
+    return (opa1)
+
+def oshibaisya(opa):
+    opa = "{0:08b}".format(opa)
+    print("Без ошибки\n",opa)
+    # ошибка рандомом
+    rng = list(range(0, 8))
+    rng1 = random.sample(rng, 2)
+    for i in range(len(rng1)):
+        if (opa[rng1[i]]) == "1":
+            opa = opa[:rng1[i]] + "0" + opa[rng1[i] + 1:]
+        else:
+            opa = opa[:rng1[i]] + "1" + opa[rng1[i] + 1:]
+        print("С ошибкой\n",opa)
+
+    return ( int(opa[:],2))
+
+
+
+# Первая часть
+def pervoe():
+    Way=name
+    os.system(Way)
+
+# #Вторая часть
+def vtoroe():
+    Way=name
+    photo=img.imread(Way)
+
+    razmer=photo.shape
+#размерность
+    y=razmer[0]
+    x=razmer[1]
 #
-#             arrara[i][j][k].append(bin(k))
-#             print(k)
+    for i in range(y):
+        for j in range(x):
+            photo[i][j][0] = int(oshibaisya(photo[i][j][0]))
+            photo[i][j][1] = int(oshibaisya(photo[i][j][1]))
+            photo[i][j][2] = int(oshibaisya(photo[i][j][2]))
+    print("Фото в матрице\n",photo)
+    plt.imshow(photo)
+    plt.axis("off")
+    plt.show()
+
+# # Третья часть
